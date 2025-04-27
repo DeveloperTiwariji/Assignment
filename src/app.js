b98d41434993f4585d1b117bd9993d4e4d0cb130
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-04-27 14:38:28 +0530

    Connect to the database successfully

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
+
+const connectDB = require("./config/database");
 
 
 app.use((req, res)=>{
@@ -8,7 +10,11 @@ app.use((req, res)=>{
 })
 
 
-
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000");
+connectDB().then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(3000, () =>{
+        console.log("Server is running on port 3000");
+    })
+}).catch((err)=>{
+    console.log("Error connecting to MongoDB", err);
 })
